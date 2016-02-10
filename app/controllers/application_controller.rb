5b15f28b3aa9414d6785c80f5bfddde1dd274702
Derek Owusu-Frimpong <deepsky_5@live.com>
2016-02-10 19:46:07 +0000

    redirect client after sign up and sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+
+  def after_sign_in_path_for(resource)
+ 	if current_client.requests.count >= 1
+ 		flash[:notice] = "Welcome back, #{current_client.email}! Please place another request."
+ 		new_request_path
+ 	elsif current_client.requests.count == 0
+ 		flash[:notice] = "Welcome back, #{current_client.email}! Please place a request."
+ 		new_request_path
+ 	else
+ 		new_request_path
+ 	end
+  end
 end
