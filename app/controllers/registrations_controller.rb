1259dfea313fc3587b9eed15570a7c52fbb9cc3e
Derek Owusu-Frimpong <deepsky_5@live.com>
2016-03-08 09:07:48 +0000

    edit routes.rb, requests_controller.rb registrations_controller.rb

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,8 +4,7 @@ class RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(resource)
   		if current_client
   		  flash[:notice] = "#{current_client.email}, welcome to workforce!"	
-  	      # new_request_path
-  	      ClientMailer.welcome_email(@client).deliver
+  	      # ClientMailer.welcome_email(@client).deliver
   	      display_request_path(Request.last)
         end  	  	
   end
--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -29,9 +29,9 @@ class RequestsController < ApplicationController
 
       if @request.save
         flash[:notice] = "#{current_client.email}, your request has been made!"
-        Client.where(admin: true).each do |recipient|
-          AdminNotifier.notification(recipient).deliver
-        end
+        # Client.where(admin: true).each do |recipient|
+        #   AdminNotifier.notification(recipient).deliver
+        # end
         redirect_to display_request_path(@request)
       else
         flash.now[:alert] = "Your request failed. Please submit it again."
@@ -44,9 +44,9 @@ class RequestsController < ApplicationController
 
       if @request.save
         flash[:notice] = "Request has been made! Please finish up by creating an account."
-        Client.where(admin: true).each do |recipient|
-          AdminNotifier.notification(recipient).deliver
-        end 
+        # Client.where(admin: true).each do |recipient|
+        #   AdminNotifier.notification(recipient).deliver
+        # end 
         redirect_to new_client_registration_path
       else
         flash.now[:alert] = "Your request failed. Please submit it again."
