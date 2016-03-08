1a7c894b4b848c0845a437af3db215364987088a
Derek Owusu-Frimpong <deepsky_5@live.com>
2016-03-08 17:16:40 +0000

    add error detection for action mailer on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,8 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'workforce16.herokuapp.com' }
 
+  config.action_mailer.raise_delivery_errors = true
+
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
