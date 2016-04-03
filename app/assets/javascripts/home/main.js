dab48a47809e6dcf5b0e1377f43c96fc28ba661a
Derek Owusu-Frimpong <deepsky_5@live.com>
2016-04-03 19:40:20 +0000

    solve precompile error

--- app/assets/javascripts/home/main.js
+++ app/assets/javascripts/home/main.js
@@ -1,8 +1,8 @@
-/*menu
-	Spectral by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
+// menu
+// 	Spectral by HTML5 UP
+// 	html5up.net | @n33co
+// 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+
 
 $(document).ready(function($) {
 
