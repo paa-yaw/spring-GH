ac34d337dcd0c88f3c2de79a42f0b8765137b065
Laud Bentil <laud.bentil@meltwater.org>
2016-04-05 08:56:09 +0000

    fix calculation

--- app/assets/javascripts/calculatePrice.js
+++ app/assets/javascripts/calculatePrice.js
@@ -1,14 +1,14 @@
 //makes the price appear in the form one page is loaded.
 
-function placeValue() {
-  if (window.location.pathname=="/requests/new" || window.location.pathname=="/requests"){
-    document.getElementById("daPrice").innerHTML = "$"+12+".00";
-    document.getElementById("daRooms").innerHTML = 4;
-    document.getElementById("daDays").innerHTML = getDaysNumber();
-  }
-// this value is returned when the function is called in calculatePricing.
-  return 12;
-}
+// function placeValue() {
+//   if (window.location.pathname=="/requests/new" || window.location.pathname=="/requests"){
+//     document.getElementById("daPrice").innerHTML = "$"+12+".00";
+//     document.getElementById("daRooms").innerHTML = 4;
+//     document.getElementById("daDays").innerHTML = getDaysNumber();
+//   }
+// // this value is returned when the function is called in calculatePricing.
+//   return 12;
+// }
 
 
 // converts value in the bedroom dropdown to integer and returns value on function call
@@ -39,129 +39,228 @@ function getHallNumber() {
   return hallNumber;
 }
 
-// converts value in Number of Days input field to integer and returns value on function call or 1 as default.
-function getDaysNumber() {
-  var daysNumber = 0;
-  var daysNumber = (parseInt(document.getElementById("request_days").value) || 1);
-  return daysNumber;
+function getRoomNumber() {
+  var rooms = (getBedroomNumber() + getBathroomNumber() + getKitchenNumber() + getHallNumber());
+  if (rooms > 4){
+    var extraRooms = parseInt(rooms-4);
+    console.log(extraRooms);
+  } else {
+    var extraRooms = 0;
+  }
+    return extraRooms;
 }
 
-function getCheckBoxValue() {
-  //declare an array to store values
-  var extraServices = [0,0,0,0];
-  
-  // if checkbox is clicked assign its value to first array position [0] else assign 0
-  if (document.getElementById('request_extra_services_8').checked == true) {
-    extraServices[0] = parseInt(document.getElementById('request_extra_services_8').value);
-  }
-  else {
+
+function getLargeRoomNumber() {
+ var rooms = (getBedroomNumber() + getBathroomNumber() + getKitchenNumber() + getHallNumber());
+ if (rooms > 8) {
+   var extraLargeRooms = parseInt(rooms - 8);
+   console.log(extraLargeRooms);
+ } else {
+  var extraLargeRooms = 0;
+ }
+ return extraLargeRooms;
+}
+
+
+function getExtraServices(){
+  var extraServices = [0,0,0];
+
+  if (document.getElementById('request_extra_services_150').checked) {
+    extraServices[0] = 15;
+  } else {
     extraServices[0] = 0;
   }
 
-  if(document.getElementById('request_extra_services_7').checked == true) {
-    extraServices[1] = parseInt(document.getElementById('request_extra_services_7').value);
-  }
-  else {
+  if (document.getElementById('request_extra_services_1001').checked) {
+    extraServices[1] = 10;
+  } else {
     extraServices[1] = 0;
   }
 
-  if (document.getElementById('request_extra_services_3').checked == true) {
-    extraServices[2] = parseInt(document.getElementById('request_extra_services_3').value);
-  }
-  else {
+  if (document.getElementById('request_extra_services_100').checked) {
+    extraServices[2] = 10;
+  } else {
     extraServices[2] = 0;
   }
 
-  if (document.getElementById('request_extra_services_5').checked == true) {
-    extraServices[3] = parseInt(document.getElementById('request_extra_services_5').value);
-  }
-  else {
-    extraServices[3] = 0;
-  }
-  return extraServices;
+  var extraServiceTotal = parseInt(extraServices[0] + extraServices[1] + extraServices[2]);
+  return extraServiceTotal;
 }
 
-function checkDays() {
-  var checkedDays = getDaysNumber();
-  var listOfDays = document.getElementsByName("request[weekdays][]");
-  
-  if(document.getElementById('request_weekdays_monday').checked == true) {
-    checkedDays -= 1;
+function getFrequency(){
+  var dayFrequency = 0;
+  var weekFrequency = 0;
+  var monthFrequency = 0;
+
+  if (document.getElementById("request_frequency_60").checked) {
+    dayFrequency = parseInt(document.getElementById("request_frequency_60").value);
   } else {
-    checkedDays +=0;
+    dayFrequency = 0;
   }
 
-  if (document.getElementById('request_weekdays_tuesday').checked == true) {
-    checkedDays -= 1;
+  if (document.getElementById("request_frequency_150").checked) {
+    weekFrequency = parseInt(document.getElementById("request_frequency_150").value);
   } else {
-    checkedDays += 0;
+    weekFrequency = 0;  
   }
 
-  if (document.getElementById('request_weekdays_wednesday').checked == true) {
-    checkedDays -= 1;
+  if (document.getElementById("request_frequency_500").checked) {
+     monthFrequency = parseInt(document.getElementById("request_frequency_500").value);
   } else {
-    checkedDays += 0;
+     monthFrequency = 0;
   }
 
-  if (document.getElementById('request_weekdays_thursday').checked == true) {
-    checkedDays -= 1;
-  } else {
-    checkedDays += 0;
-  }
+  var totalFrequency = (dayFrequency + weekFrequency + monthFrequency);
+  return totalFrequency;
+}
+
+
+// converts value in Number of Days input field to integer and returns value on function call or 1 as default.
+// function getDaysNumber() {
+//   var daysNumber = 0;
+//   var daysNumber = (parseInt(document.getElementById("request_days").value) || 1);
+//   return daysNumber;
+// }
+
+// function getCheckBoxValue() {
+//   //declare an array to store values
+//   var extraServices = [0,0,0,0];
   
-  if (document.getElementById('request_weekdays_friday').checked == true) {
-    checkedDays -= 1;
-  } else {
-    checkedDays += 0;
-  }
+//   // if checkbox is clicked assign its value to first array position [0] else assign 0
+//   if (document.getElementById('request_extra_services_8').checked == true) {
+//     extraServices[0] = parseInt(document.getElementById('request_extra_services_8').value);
+//   }
+//   else {
+//     extraServices[0] = 0;
+//   }
+
+//   if(document.getElementById('request_extra_services_7').checked == true) {
+//     extraServices[1] = parseInt(document.getElementById('request_extra_services_7').value);
+//   }
+//   else {
+//     extraServices[1] = 0;
+//   }
+
+//   if (document.getElementById('request_extra_services_3').checked == true) {
+//     extraServices[2] = parseInt(document.getElementById('request_extra_services_3').value);
+//   }
+//   else {
+//     extraServices[2] = 0;
+//   }
+
+//   if (document.getElementById('request_extra_services_5').checked == true) {
+//     extraServices[3] = parseInt(document.getElementById('request_extra_services_5').value);
+//   }
+//   else {
+//     extraServices[3] = 0;
+//   }
+//   return extraServices;
+// }
+
+// function checkDays() {
+//   var checkedDays = 3;
+//   var listOfDays = document.getElementsByName("request_weekdays");
   
-  if (document.getElementById('request_weekdays_saturday').checked == true) {
-    checkedDays -= 1;
-  } else {
-    checkedDays += 0;
-  }
+//   if(document.getElementById('request_weekdays_monday').checked == true) {
+//     checkedDays -= 1;
+//   } else {
+//     checkedDays +=0;
+//   }
+
+//   if (document.getElementById('request_weekdays_tuesday').checked == true) {
+//     checkedDays -= 1;
+//   } else {
+//     checkedDays += 0;
+//   }
+
+//   if (document.getElementById('request_weekdays_wednesday').checked == true) {
+//     checkedDays -= 1;
+//   } else {
+//     checkedDays += 0;
+//   }
+
+//   if (document.getElementById('request_weekdays_thursday').checked == true) {
+//     checkedDays -= 1;
+//   } else {
+//     checkedDays += 0;
+//   }
+  
+//   if (document.getElementById('request_weekdays_friday').checked == true) {
+//     checkedDays -= 1;
+//   } else {
+//     checkedDays += 0;
+//   }
+  
+//   if (document.getElementById('request_weekdays_saturday').checked == true) {
+//     checkedDays -= 1;
+//   } else {
+//     checkedDays += 0;
+//   }
     
-  if (document.getElementById('request_weekdays_sunday').checked == true) {
-    checkedDays -= 1;
-  } else {
-    checkedDays += 0;
-  }
+//   if (document.getElementById('request_weekdays_sunday').checked == true) {
+//     checkedDays -= 1;
+//   } else {
+//     checkedDays += 0;
+//   }
   
-  if (checkedDays < 0) {
-    alert("You can only choose "+getDaysNumber()+" days");
-    for (var i = 0; i < listOfDays.length; i++) {
-      listOfDays[i].checked = false;
-    };
-  }
-  console.log(checkedDays);
-}
+//   if (checkedDays < 0) {
+//     alert("You can only choose 3 days");
+//     for (var i = 0; i < listOfDays.length; i++) {
+//       listOfDays[i].checked = false;
+//     };
+//   }
+//   console.log(checkedDays);
+// }
 
-function getCleaningProduct() {
-  var cleaningProduct = 0;
-  if(document.getElementsByName('request[provide]')[0].checked == true ) {
-    cleaningProduct = 3;
-  } else if(document.getElementsByName('request[provide]')[1].checked == true ) {
-    cleaningProduct = 0;
-  }
-  return cleaningProduct;
-}
 
 // calculates pricing based on number of rooms.
+
 function calculatePricing() {
-  checkDays();
-  // if a cleaning product is provided for or 
-  var cleanProduct = (getCleaningProduct()*getDaysNumber());
-  // extra services multiplying the number of days
-  var extra = ((getCheckBoxValue()[0]+getCheckBoxValue()[1]+getCheckBoxValue()[2]+getCheckBoxValue()[3])*getDaysNumber());
-  // add up all the rooms.
-  var rooms = (getBedroomNumber() + getBathroomNumber() + getKitchenNumber() + getHallNumber());
-  document.getElementById("daRooms").innerHTML = rooms;
-  if(rooms > 4){
-    // if rooms is more than 4 calculate pricing with number of days as a multiple.
-    var pricing = (((((rooms-4)*5)+12)*getDaysNumber())+extra+cleanProduct);
-    document.getElementById("daPrice").innerHTML = "$"+pricing+".00";
+  // checkDays();
+  getLargeRoomNumber();
+  getRoomNumber();
+  getExtraServices();
+  getFrequency();
+
+  document.getElementById('packagePrice').innerHTML = "GHc"+getFrequency()+".00"
+  document.getElementById('extraServicePrice').innerHTML = "GHc"+getExtraServices()+".00";
+
+  if (getFrequency() == 500) {
+    document.getElementById("extraRooms").innerHTML =( 0 || getLargeRoomNumber() );
   } else {
-    // if rooms is equal to 4 or less use 45 as default and calculate pricing.
-    document.getElementById("daPrice").innerHTML = "$"+((placeValue()*(parseInt(getDaysNumber())))+extra+cleanProduct)+".00"; 
+    document.getElementById("extraRooms").innerHTML =( 0 || getRoomNumber() );
   }
+   
+  if (getFrequency() == 500) {
+    document.getElementById("extraRoomPrice").innerHTML = "GHc"+getLargeRoomNumber()*5+".00";
+  } else{
+    document.getElementById("extraRoomPrice").innerHTML = "GHc"+getRoomNumber()*5+".00";
+  }
+
+  if (getFrequency() == 60){
+    document.getElementById("roomNumber").innerHTML = 4;
+    var total = 60 + getRoomNumber()*5 + getExtraServices(); 
+    console.log(total);
+    document.getElementById("totalPrice").innerHTML = "GHc"+total;
+  }
+  else if (getFrequency() == 150) {
+    document.getElementById("roomNumber").innerHTML = 4;
+    var total = 150 + getRoomNumber()*5 + getExtraServices();
+    console.log(total);
+    document.getElementById("totalPrice").innerHTML = "GHc"+total;
+  }
+  else if (getFrequency() == 500) {
+    var rooms = (getBedroomNumber() + getBathroomNumber() + getKitchenNumber() + getHallNumber());
+    if (rooms >=8){
+    document.getElementById("roomNumber").innerHTML = 8;
+    } else {
+    document.getElementById("roomNumber").innerHTML = 4;
+    }
+
+    var total = 500 + getLargeRoomNumber()*5 + getExtraServices();
+    console.log(total);
+    document.getElementById("totalPrice").innerHTML = "GHc"+total;
+  }
+
 }
