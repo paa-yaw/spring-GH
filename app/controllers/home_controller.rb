41d724100516b9b1b15ba5b0d333e270fc382d23
Derek Owusu-Frimpong <deepsky_5@live.com>
2016-04-14 16:09:43 +0000

    add letsencrypt

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  
+ #for letsencrypt  
   def letsencrypt
   	render text: "auvR79nrHktzbnArSdTiI8VtVB3LjhVksROklm8BDyg.ji9RDrr3LRp1puJfrHOaEtDyJG3vt9wnuHG6vkze-SQ
 "
