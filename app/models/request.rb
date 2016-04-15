da59dd7594fabfdf7d73cbc66fc5b0c958c2da33
Derek Owusu-Frimpong <deepsky_5@live.com>
2016-04-15 16:15:22 +0000

    add date time validation

--- app/models/request.rb
+++ app/models/request.rb
@@ -24,14 +24,16 @@ class Request < ActiveRecord::Base
     end
 
     def forbidden_dates
-      if date_time.month < Time.now.month
-      	errors.add(:date_time, "Forbidden date")
-      elsif date_time.month > Time.now.month + 3
-      	errors.add(:date_time, "Forbidden date")      		
+      if date_time.year == 2016 && date_time.month < Time.now.month
+      	errors.add(:date_time, "Can't choose a month in the past")
+      # elsif date_time.month > Time.now.month + 3
+      # 	errors.add(:date_time, "Can't choose a month beyond 3 months from now.")      		
       elsif date_time.month == Time.now.month && date_time.day < Time.now.day
-      	errors.add(:date_time, "Forbidden date")
+      	errors.add(:date_time, "Can't choose a day in the past.")
       elsif date_time.month == Time.now.month && date_time.hour < Time.now.hour
-      	errors.add(:date_time, "Forbidden date")      		
+      	errors.add(:date_time, "Can't choose a time in the past.")
+      elsif date_time.year == 2017 && date_time.month < 10
+      	errors.add(:date_time, "the year is too far off")
       end
     end
 
