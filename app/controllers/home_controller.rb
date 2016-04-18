63e63fd7df535e675df6c5c5242e1b88e1b07c2b
Derek Owusu-Frimpong <deepsky_5@live.com>
2016-04-18 11:39:20 +0000

    add https to www.springgh.com and springgh.com

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
  #for letsencrypt  
   def letsencrypt
-  	render text: "cGu5eFwj92OVTSrH-mwZ-2FJdMgoYNmxiVeaKs2Ilt8.ji9RDrr3LRp1puJfrHOaEtDyJG3vt9wnuHG6vkze-SQ
+  	render text: "CxavN2oZrGO6WRY9LCXxx3N2nDJ-VmIwJj-8dzz41yc.ji9RDrr3LRp1puJfrHOaEtDyJG3vt9wnuHG6vkze-SQ
 "
   end
 
