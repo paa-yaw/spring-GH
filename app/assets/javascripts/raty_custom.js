0b9dd004685f9ed9e24b728f9b30c57a2eba7a72
Derek Owusu-Frimpong <deepsky_5@live.com>
2016-04-21 09:31:47 +0000

    fix raty

--- app/assets/javascripts/raty_custom.js
+++ app/assets/javascripts/raty_custom.js
@@ -1,17 +1,17 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
+// console.log("&".repeat(100));
 
+// $('#star-rating').raty({
+// 	path: '/assets/',
+// 	scoreName: 'review[rating]'
+// });
 
-$('#star-rating').raty({
-	path: '/assets/',
-	scoreName: 'review[rating]'
-});
-
-$('.star-rating').raty({
-    path: '/assets/', 
-    readOnly: true,
-    score: function() {
-      return $(this).attr('data-score');
-    }
-  });
-});
+// $('.star-rating').raty({
+//     path: '/assets/', 
+//     readOnly: true,
+//     score: function() {
+//       return $(this).attr('data-score');
+//     }
+//   });
+// });
--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,2 +1,19 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function(){
+
+console.log("&".repeat(100));
+
+$('#star-rating').raty({
+	path: '/assets/',
+	scoreName: 'review[rating]'
+});
+
+$('.star-rating').raty({
+    path: '/assets/', 
+    readOnly: true,
+    score: function() {
+      return $(this).attr('data-score');
+    }
+  });
+});
