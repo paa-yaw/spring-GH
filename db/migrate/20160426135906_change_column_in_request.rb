3c43bfe944676de1b68950fe28deea580b13c888
Derek Owusu-Frimpong <deepsky_5@live.com>
2016-04-26 19:20:17 +0000

    changes

--- db/migrate/20160426135906_change_column_in_request.rb
+++
@@ -1,6 +0,0 @@
-class ChangeColumnInRequest < ActiveRecord::Migration
-  def change
-  	remove_column :requests, :frequency, :integer
-  	add_column :requests, :frequency, :decimal, precision: 8, scale: 2
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160426135906) do
+ActiveRecord::Schema.define(version: 20160423154410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,22 +58,22 @@ ActiveRecord::Schema.define(version: 20160426135906) do
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
   create_table "requests", force: :cascade do |t|
-    t.datetime "created_at",                                                    null: false
-    t.datetime "updated_at",                                                    null: false
+    t.datetime "created_at",                            null: false
+    t.datetime "updated_at",                            null: false
     t.integer  "client_id"
     t.datetime "date_time"
-    t.boolean  "resolved",                               default: false
-    t.integer  "bedrooms",                               default: 0
-    t.integer  "bathrooms",                              default: 0
-    t.integer  "kitchens",                               default: 0
-    t.integer  "hall",                                   default: 0
-    t.string   "weekdays",                               default: [],                        array: true
-    t.decimal  "extra_services",                         default: [],                        array: true
-    t.string   "status",                                 default: "unresolved"
+    t.boolean  "resolved",       default: false
+    t.integer  "bedrooms",       default: 0
+    t.integer  "bathrooms",      default: 0
+    t.integer  "kitchens",       default: 0
+    t.integer  "hall",           default: 0
+    t.string   "weekdays",       default: [],                        array: true
+    t.decimal  "extra_services", default: [],                        array: true
+    t.string   "status",         default: "unresolved"
     t.string   "email"
     t.string   "phone_number"
     t.string   "location"
-    t.decimal  "frequency",      precision: 8, scale: 2
+    t.integer  "frequency"
   end
 
   add_index "requests", ["client_id"], name: "index_requests_on_client_id", using: :btree
