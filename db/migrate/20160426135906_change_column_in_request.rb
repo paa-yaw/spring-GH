a702c2012a6f8ac3cab7409cfc28501de820e765
Derek Owusu-Frimpong <deepsky_5@live.com>
2016-04-26 18:59:07 +0000

    changes to schema

--- db/migrate/20160426135906_change_column_in_request.rb
+++ db/migrate/20160426135906_change_column_in_request.rb
@@ -1,6 +1,6 @@
 class ChangeColumnInRequest < ActiveRecord::Migration
   def change
   	remove_column :requests, :frequency, :integer
-  	add_column :requests, :frequency, :decimal, precision: 5, scale: 2
+  	add_column :requests, :frequency, :decimal, precision: 8, scale: 2
   end
 end
