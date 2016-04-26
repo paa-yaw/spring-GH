426af3cbaca62c0f6258aec683f56a7126f5d81e
Derek Owusu-Frimpong <deepsky_5@live.com>
2016-04-26 21:14:36 +0000

    new tls cert

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
  #for letsencrypt  
   def letsencrypt
-  	render text: "pjENMFS0J42rMZrVuCbBo0QInAxWIW46g0ZdybxY8Oo.ji9RDrr3LRp1puJfrHOaEtDyJG3vt9wnuHG6vkze-SQ
+  	render text: "Mxd2frVRPvnFRKTkuPExOG1kvqjRWJDbzlW_vnPrhPc.ji9RDrr3LRp1puJfrHOaEtDyJG3vt9wnuHG6vkze-SQ
 "
   end
 
