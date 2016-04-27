59b0874aa49d830f8df0979ba43b63f1a30bd0f2
Derek Owusu-Frimpong <deepsky_5@live.com>
2016-04-27 14:36:40 +0000

    resolving issues

--- app/models/request.rb
+++ app/models/request.rb
@@ -26,8 +26,8 @@ class Request < ActiveRecord::Base
     def forbidden_dates
       if date_time.year == Time.now.year && date_time.month < Time.now.month
       	errors.add(:date_time, "Can't choose a month in the past")
-      elsif date_time.year == Time.now.year && date_time.month == Time.now.month && date_time.day == Time.now.day && date_time.hour == Time.now.hour
-        errors.add(:date_time, "you cannot schedule for today.")   
+      elsif date_time.year == Time.now.year && date_time.month == Time.now.month && date_time.day == Time.now.day 
+        errors.add(:date_time, "you cannot schedule for today. Please book from tomorrow onwards.")   
       elsif date_time.year == Time.now.year && date_time.month > Time.now.month + 3
       	errors.add(:date_time, "Can't choose a month beyond 3 months from now.")      		
       elsif date_time.year == Time.now.year && date_time.month == Time.now.month && date_time.day < Time.now.day
