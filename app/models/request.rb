ed6c7995bfa1bf8e7a2d5c35ab447e5f40b99a42
Derek Owusu-Frimpong <deepsky_5@live.com>
2016-04-29 02:40:22 +0000

    error message edit

--- app/models/request.rb
+++ app/models/request.rb
@@ -28,23 +28,23 @@ class Request < ActiveRecord::Base
 
     def forbidden_dates
       if date_time.year == Time.now.year && date_time.month < Time.now.month
-      	errors.add(:date_time, "Can't choose a month in the past")
+      	errors.add(:date_time, ": Can't choose a month in the past")
       elsif date_time.year == Time.now.year && date_time.month == Time.now.month && date_time.day == Time.now.day 
-        errors.add(:date_time, "you cannot schedule for today. Please book from tomorrow onwards.")   
+        errors.add(:date_time, ": you cannot schedule for today. Please book from tomorrow onwards.")   
       elsif date_time.year == Time.now.year && date_time.month > Time.now.month + 3
-      	errors.add(:date_time, "Can't choose a month beyond 3 months from now.")      		
+      	errors.add(:date_time, ": Can't choose a month beyond 3 months from now.")      		
       elsif date_time.year == Time.now.year && date_time.month == Time.now.month && date_time.day < Time.now.day
-      	errors.add(:date_time, "Can't choose a day in the past.")
+      	errors.add(:date_time, ": Can't choose a day in the past.")
       elsif  date_time.year == Time.now.year && (date_time.month == Time.now.month || date_time.day == Time.now.day) && (date_time.hour < 6 || date_time.hour > 17)
-      	errors.add(:date_time, "service is not available at this time. Choose a time between 6am & 5pm")
+      	errors.add(:date_time, ": service is not available at this time. Choose a time between 6am & 5pm")
       elsif  date_time.year == Time.now.year && (date_time.hour < 6 || date_time.hour > 17)
-      	errors.add(:date_time, "service is not available at this time. Choose a time between 6am & 5pm")
+      	errors.add(:date_time, ": service is not available at this time. Choose a time between 6am & 5pm")
       elsif date_time.day == Time.now.day && date_time.year == Time.now.year && date_time.month == Time.now.month && (date_time.hour > 6 && date_time.hour < 17)
         errors.add(:date_time, "can't choose time in the past.") if date_time.hour < Time.now.hour		
       elsif date_time.year == Time.now.year + 1 && date_time.month < 10
-      	errors.add(:date_time, "the date selected is too far off")
+      	errors.add(:date_time, ": the date selected is too far off")
       elsif date_time.year == Time.now.year + 1
-      	errors.add(:date_time, "the date selected is too far off")	      			      		
+      	errors.add(:date_time, ": the date selected is too far off")	      			      		
       end
     end
 
