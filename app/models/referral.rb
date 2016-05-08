c6f4939951ce1b85e203076dcfc4574273256dc9
Derek Owusu-Frimpong <deepsky_5@live.com>
2016-05-08 01:11:13 +0000

    commit fix

--- app/models/referral.rb
+++ app/models/referral.rb
@@ -1,6 +1,7 @@
 class Referral < ActiveRecord::Base
   belongs_to :sender, :class_name => "Client"
   belongs_to :recipient, :class_name => "Client"
-
+  
+  validates_format_of :email,:with => Devise::email_regexp, allow_blank: true
   validates :email, uniqueness: {message: "you have already invited this person"}
 end
