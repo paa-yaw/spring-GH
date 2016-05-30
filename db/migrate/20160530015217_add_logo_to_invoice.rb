ac13b45bcbf07119861241e47b07e32684540e36
Derek Owusu-Frimpong <deepsky_5@live.com>
2016-05-30 09:09:49 +0000

    effect migration on staging

--- db/migrate/20160530015217_add_logo_to_invoice.rb
+++ db/migrate/20160530015217_add_logo_to_invoice.rb
@@ -1,6 +1,6 @@
 class AddLogoToInvoice < ActiveRecord::Migration
   def change
-  	add_column :invoices, :logo, :string
+  	# add_column :invoices, :logo, :string
   	remove_column :invoices, :logo, :string
   end
 end
