4e7d78c825fcc93a869bf682bf3a98d61acf6270
Derek Owusu-Frimpong <deepsky_5@live.com>
2016-05-30 02:59:56 +0000

    resize logo on email

--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -39,7 +39,7 @@ class LogoUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [100, 100]
+    process :resize_to_fit => [150, 150]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
