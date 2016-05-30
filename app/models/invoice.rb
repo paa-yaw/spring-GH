aa629d4583623e1bd5d250bbf46b02431a58326a
Derek Owusu-Frimpong <deepsky_5@live.com>
2016-05-30 07:10:56 +0000

    changese

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,8 +1,6 @@
 class Invoice < ActiveRecord::Base
   belongs_to :request
 
-  mount_uploader :logo, LogoUploader
-
   validates_format_of :recipient_email,:with => Devise::email_regexp
   validates :invoice_to, :invoice_from, :due_balance, :amount_paid,  presence: true
 end
